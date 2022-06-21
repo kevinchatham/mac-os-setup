806f9cf96382bf6976a40e09e4f75a0601772959
kevinchatham <40923272+kevinchatham@users.noreply.github.com>
2022-06-21 18:46:29 -0500

    backup

---
+++ backup-plist.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cp ~/Library/Preferences/com.apple.dock.plist ~/Git/mac-os-setup/com.apple.dock.plist
+cp ~/Library/Preferences/com.amethyst.Amethyst.plist ~/Git/mac-os-setup/com.amethyst.Amethyst.plist
+cp ~/Library/Preferences/org.keepassxc.keepassxc.plist ~/Git/mac-os-setup/
